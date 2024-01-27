efb0079667c52a076bac4e2c9ff7a449956cadaf
Alek V <alekvasek@icloud.com>
2024-01-27 10:11:56 -0600

    Updated Code

--- js/main.js
+++ js/main.js
@@ -75,3 +75,18 @@ function openLink(search_engine, id) {
     }
     
 }
+
+function copiy(id) {
+    // Get the text field
+    var copyText = document.getElementById(id);
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Text Copied!");
+}
